488d505db88d9b5df2aa1648b732364e2a7b6da4
advityasingh <56801642+advityasingh@users.noreply.github.com>
2019-10-23 10:16:35 +0530

    Add files via upload

---
+++ POINTER call by value and reference.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int increment(int var)
+{
+    var = var+1;
+    return var;
+}
+
+int main()
+{
+   int num1=93;
+   int num2 = increment(num1);
+   printf("num1 value is: %d", num1);
+   printf("\nnum2 value is: %d", num2);
+
+   return 0;
+}
+
+
